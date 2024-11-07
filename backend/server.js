f8210fa22b03dcdc94833b8c76bce4c44e83f2e7
Akash Singh <66031093+akashsingh6474@users.noreply.github.com>
2024-11-07 22:47:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import connectDB from './config/db.js';
 import { PORT } from './config/utils.js';
 import { connectToRedis } from './services/redis.js';
 
-const port = PORT || 8080;
+const port = PORT || 5050;
 
 // Connect to redis
 connectToRedis();
